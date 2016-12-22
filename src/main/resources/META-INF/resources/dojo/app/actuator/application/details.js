d6898a8ac91ee696f3a41ccf432e70b560b170a2
zhanhongbo1112 <eric.zhan@163.com>
2016-12-22 22:55:39 +0800

    update dashboard page

--- src/main/resources/META-INF/resources/dojo/app/actuator/application/details.js
+++ src/main/resources/META-INF/resources/dojo/app/actuator/application/details.js
@@ -1,21 +1,53 @@
-define(['jquery', 'jquery/counter/waypoints', 'jquery/counter/counterup'], function () {
+define(['dojo/_base/lang', 'baf/html/form', 'app/actuator/endpoints', 'jquery'], function (lang, form, endpoints) {
+    var application = form.formToObject('form');
+
     return {
         startup: function () {
-            $('.counter').counterUp({
-                delay: 10,
-                time: 1000
+            var _this = this;
+            $('.tab ul li').on('click', function () {
+                var endpoint;
+                if ($(this).hasClass('info')) {
+                    endpoint = endpoints.INFO;
+                } else if ($(this).hasClass('configuredProperties')) {
+                    endpoint = endpoints.CONFIG_PROPS;
+                } else if ($(this).hasClass('dump')) {
+                    endpoint = endpoints.DUMP;
+                } else if ($(this).hasClass('health')) {
+                    endpoint = endpoints.HEALTH;
+                } else if ($(this).hasClass('mappings')) {
+                    endpoint = endpoints.MAPPINGS;
+                } else if ($(this).hasClass('metrics')) {
+                    endpoint = endpoints.METRICS;
+                } else if ($(this).hasClass('beans')) {
+                    endpoint = endpoints.BEANS;
+                } else if ($(this).hasClass('environments')) {
+                    endpoint = endpoints.ENVIRONMENT;
+                } else if ($(this).hasClass('autoConfigurations')) {
+                    endpoint = endpoints.AUTO_CONFIG;
+                } else if ($(this).hasClass('trace')) {
+                    endpoint = endpoints.TRACE;
+                }
+
+                console.log('invoke endpoint: ' + endpoint);
+                if (endpoint) {
+                    _this.refresh(endpoint);
+                }
             });
 
-            console.log(this._formToObject('form'));
+            $('.tab ul li.active').click();
         },
 
-        _formToObject: function (formId) {
-            var result = {};
-            $('#' + formId + ' [name]').each(function () {
-                result[this.name] = this.value;
-            });
+        refresh: function (endpoint) {
+            $.get(application['url'] + endpoint, lang.hitch(this, this._onRefresh));
+        },
 
-            return result;
+        _onRefresh: function (data, status) {
+            console.log('status: ' + status);
+            $('.tab-content .panel-body').html(this._format(data));
+        },
+
+        _format: function (data) {
+            return '<pre class="console">' + JSON.stringify(data, null, 2) + '</pre>';
         }
     };
 });
---
+++ src/main/resources/META-INF/resources/dojo/app/actuator/endpoints.js
@@ -0,0 +1,14 @@
+define([], function () {
+    return {
+        INFO: "/info",
+        CONFIG_PROPS: "/configprops",
+        DUMP: "/dump",
+        HEALTH: "/health",
+        MAPPINGS: "/mappings",
+        METRICS: "/metrics",
+        BEANS: "/beans",
+        ENVIRONMENT: "/env",
+        AUTO_CONFIG: "/autoconfig",
+        TRACE: "/trace"
+    }
+});
